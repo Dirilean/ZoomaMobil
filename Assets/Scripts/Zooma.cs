dbde7aa962b108f0a9903943260b089d9c09b614
Dirilean <gaieva-diana@mail.ru>
2020-06-15 10:12:32 +0500

    Start

---
+++ Assets/Scripts/Zooma.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zooma : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float angle = 0;
+
+
+    // public Camera camera;
+    // Update is called once per frame
+    void Update()
+    {
+        LookOnCursor();
+    }
+    void LookOnCursor()
+    {     
+        //заставляет персонажа следить за курсором мышки
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitdist = 0;
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+            transform.rotation = Quaternion.LookRotation(targetPoint - transform.position);
+        }
+    }
+}
